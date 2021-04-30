a7e8637bdd1522fb1c9135ff41b601e4ebc2cb01
Alexandre Martins <martinso.alex@gmail.com>
2021-04-30 10:40:20 -0300

    react-router-dom switch

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import './assets/css/base/base.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Home from "./paginas/Home";
+import Sobre from "./paginas/Sobre";
+
+import "./assets/css/base/base.css";
 
 function App() {
-  return (
-  <>
-
-  </>
-  )
+	return (
+		<Router>
+			<Switch>
+				<Route exact path='/'>
+					<Home />
+				</Route>
+				<Route path='/sobre'>
+					<Sobre />
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
-export default App
+export default App;
