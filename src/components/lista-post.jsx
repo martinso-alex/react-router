81e704cf866e662f423730ff2f50ec7f29adef31
Alexandre Martins <martinso.alex@gmail.com>
2021-04-30 12:17:19 -0300

    consumindo fake apir de posts

---
+++ src/components/lista-post.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { busca } from "../api/api";
+
+export const ListaPost = (props) => {
+	const [posts, setPosts] = useState([]);
+
+	const { url } = props;
+
+	useEffect(() => {
+		async function buscaPosts() {
+			return await busca(url, setPosts);
+		}
+
+		buscaPosts();
+	});
+
+	return (
+		<section className='posts container'>
+			{posts.map((post) => (
+				<Link className={`cartao-post cartao-post--${post.categoria}`}>
+					<article key={post.id}>
+						<h3 className='cartao-post__titulo'>{post.title}</h3>
+						<p className='cartao-post__meta'>{post.metadescription}</p>
+					</article>
+				</Link>
+			))}
+		</section>
+	);
+};
