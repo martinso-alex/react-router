473c92a270dcfcfbed4b13b8f381fc73e4b2a97c
Alexandre Martins <martinso.alex@gmail.com>
2021-04-30 11:24:05 -0300

    navbar navigation

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,24 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./paginas/Home";
 import Sobre from "./paginas/Sobre";
 import Page404 from "./paginas/404";
+import { Cabecalho } from "./components/cabecalho";
 
 import "./assets/css/base/base.css";
 
 function App() {
 	return (
 		<Router>
+			<Cabecalho />
+
 			<Switch>
 				<Route exact path='/'>
 					<Home />
 				</Route>
+
 				<Route path='/sobre'>
 					<Sobre />
 				</Route>
+
 				<Route>
 					<Page404 />
 				</Route>
